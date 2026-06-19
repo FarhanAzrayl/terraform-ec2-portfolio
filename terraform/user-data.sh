de3ad33ab06599cdf052c33dde2f01ef889eb9e3
FarhanAzrayl <sam_farhan94@yahoo.com>
2026-06-19 22:57:41 +0800

    Update user-data.sh

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+set -eux
+
 apt update -y
-apt install nginx -y
+apt install -y nginx curl
 
-systemctl start nginx
 systemctl enable nginx
+systemctl start nginx
 
-cat <<EOF > /var/www/html/index.html
-$(cat /tmp/index.html)
-EOF
+# remove ALL default nginx pages
+rm -rf /var/www/html/*
+
+# download your GitHub HTML
+curl -L --fail -o /var/www/html/index.html \
+https://raw.githubusercontent.com/FarhanAzrayl/terraform-ec2-portfolio/main/Website/index.html
+
+# fix permissions
+chown -R www-data:www-data /var/www/html
+
+# restart nginx
+systemctl restart nginx
